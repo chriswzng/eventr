e37df397ebc7ecc1c2a52f1f25ac73d7d47bb632
Chris Ng <weizhi.ng@gmail.com>
2015-06-13 20:47:57 +0800

    Rent venue, Host venue, Host service pages 1st phase

--- angelhack/androidIonic/myApp2/www/template/loginController.js
+++ angelhack/androidIonic/myApp2/www/template/loginController.js
@@ -10,11 +10,18 @@ getLoginStatus();
 
 $scope.login = function() {
       // From now on you can use the Facebook service just as Facebook api says
+      $scope.loggedIn = true;
+      $state.go('getStarted');
+    };
+
+$scope.fbLogin = function() {
+      // From now on you can use the Facebook service just as Facebook api says
       Facebook.login(function(response) {
         // Do something with response.
         var test = response;
       });
     };
+
 function getLoginStatus() {
       Facebook.getLoginStatus(function(response) {
         if(response.status === 'connected') {
@@ -27,3 +34,4 @@ function getLoginStatus() {
     };
 
 	});
+
