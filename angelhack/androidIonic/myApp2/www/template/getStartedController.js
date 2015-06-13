95a7e80b8dc5e5cfbfe33b7ce89201b543744222
shangyi <shangyilim@gmail.com>
2015-06-13 21:22:38 +0800

    added linkage to getstarted

--- angelhack/androidIonic/myApp2/www/template/getStartedController.js
+++ angelhack/androidIonic/myApp2/www/template/getStartedController.js
@@ -1,10 +1,12 @@
 angular.module('starter')
-.controller('GetStartedController', function($scope, $ionicModal) {
+.controller('GetStartedController', function($scope, $ionicModal, $state) {
 
 $scope.contact = {
     name: 'Mittens Cat2',
     info: 'Tap anywhere on the card to open the modal'
   }
-
+$scope.rentVenue = function(){
+  $state.go('venueFinder');
+}
 
 	});
