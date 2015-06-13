f9f4c2c42036085125d1b075233df840819317e5
shangyi <shangyilim@gmail.com>
2015-06-14 01:04:45 +0800

    updates to venueviewer

--- angelhack/androidIonic/myApp2/www/template/venueViewerController.js
+++ angelhack/androidIonic/myApp2/www/template/venueViewerController.js
@@ -1,11 +1,24 @@
 angular.module('starter')
 .controller('VenueViewerController', function($scope, $ionicModal, $state, $http) {
 
+getData();
+
 function getData(){
     var venueId = $state.params.venueId;
 
     if(venueId != "" && venueId != undefined){
 
+      $http.get('json/venues.json').success(function(data){
+        
+
+          data.forEach(function(item){
+
+                if(item.id == venueId){
+                  $scope.selectedVenue = item;
+                }
+            })
+
+      });
 
     }
 }
